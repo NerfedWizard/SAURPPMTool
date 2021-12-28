cf23109f7c8c9e0341fb92cb390ff3957c4587ae
NerfedWizard <loelnelson@gmail.com>
2021-12-28 17:11:02 -0600

    Trying some more

---
+++ PPMToolFullStack1/src/test/java/com/loel/PpmToolApplicationTests.java
@@ -0,0 +1,15 @@
+
+package com.loel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
