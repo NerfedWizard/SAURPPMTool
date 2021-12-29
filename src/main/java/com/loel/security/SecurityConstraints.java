6fd246f958a84b0d953c2c1b4d6f007b5b723913
NerfedWizard <loelnelson@gmail.com>
2021-12-28 18:42:43 -0600

    Should be the final

--- src/main/java/com/loel/security/SecurityConstraints.java
+++ src/main/java/com/loel/security/SecurityConstraints.java
@@ -8,6 +8,6 @@ public class SecurityConstraints {
 	public static final String SECRET = "SecretKeyToGenJWTs";
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
-	public static final long EXPIRATION_TIME = 3600000; // 1 Hour
+	public static final long EXPIRATION_TIME = 3600000; // 1 Hour before WebToken Expires
 
 }
