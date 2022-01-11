b74f058b25331d0a9f5183963cb4e254d8873eba
NerfedWizard <loelnelson@gmail.com>
2022-01-11 07:20:07 -0600

    Maybe this will deploy

--- src/main/java/com/loel/exceptions/ProjectIDException.java
+++ src/main/java/com/loel/exceptions/ProjectIDException.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProjectIDException extends RuntimeException {
-
-	/**
-	 *
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public ProjectIDException(String message) {
